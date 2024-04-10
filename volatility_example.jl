73e8e2354bf0cf3c7e21c967e8cca7940553d64e
fmeulen <frankvdm0@gmail.com>
2024-04-10 11:24:14 +0200

    forward guide added

--- volatility_example.jl
+++ volatility_example.jl
@@ -7,21 +7,23 @@ using Plots
 using UnPack
 
 ### Parameters
+p = (ω =  0.0, ψ = 0.9, η=0.363,μ=-1.27, σ=√((π^2)/2))
+# with larger variance
 mult_var = 3.0
-p = (ω =  0.0, ψ = 0.9, η=0.363,μ=-1.27,
-         σ=√((π^2)/2), modσ = √(mult_var*(π^2)/2))
+p_mv = (ω =  0.0, ψ = 0.9, η=0.363,μ=-1.27, σ = √(mult_var*(π^2)/2))
+
+# Forward simulate
+forward(x, p) = p.ω + p.ψ *x + p.η * randn()
 
 function xsim(x0, S, p)
   x = fill(x0, S+1)
   for i ∈ 2:S+1
-    x[i] = p.ω + p.ψ *x[i - 1] + p.η * randn()
+    x[i] = forward(x[i-1], p)
   end
   x[2:end]
 end
 
 Random.seed!(10)
-
-
 x0 = rand(Normal(p.ω/(1.0 - p.ψ), sqrt(p.η^2 / (1 - p.ψ^2))))    # Sample x0 from the stationary distribution 
 S = 500 # Correct for time 0 (called tot_steps)  (Time steps)
 
@@ -35,6 +37,8 @@ p1 = plot(X, label="X", title="latent")
 p2 = plot(V, label="V", title="observed")
 plot(p1, p2)
 
+
+# Backward filtering
 struct Message{Tc, TF, TH}
     c::Tc
     F::TF
@@ -45,10 +49,11 @@ fuse(m1::Message, m2::Message) = Message(m1.c+m2.c, m1.F+m2.F, m1.H+m2.H)
 
 function pullback(m::Message, p)
     @unpack c, F, H = m
-    C = p.η^2 + 1/H 
-    Hnew = p.ψ^2 / C
-    Fnew = p.ψ*(F/H - p.ω)/C
-    cnew = c - logpdf(NormalCanon(F,H),0) + logpdf(Normal(F/H,C),p.ω)
+    @unpack ω, ψ, η, μ, σ = p
+    C = η^2 + 1/H 
+    Hnew = ψ^2 / C
+    Fnew = ψ*(F/H - ω)/C
+    cnew = c - logpdf(NormalCanon(F,H),0) + logpdf(Normal(F/H,C),ω)
     Message(cnew, Fnew, Hnew)
 end   
 
@@ -69,5 +74,34 @@ end
 
 
 bf = backwardfilter(V,p)
+bf_mv = backwardfilter(V,p_mv)
 
 
+function guide(x, m::Message, p) 
+    @unpack c, F, H = m
+    @unpack ω, ψ, η, μ, σ = p
+    μg = F/H
+    σg2 = H^(-1)
+    η2 = η^2
+    μx = ω + ψ * x
+    μ̃ = (μx*σg2 + μg*η2)/(η2 + σg2)
+    σ̃ = sqrt((η2*σg2)/(η2+σg2))
+    rand(Normal(μ̃, σ̃))
+end
+
+logweight(x, m::Message) = m.c + m.F * x + 0.5*x*m.H*x
+
+function forwardguide(x0, bf, p; ϵ=0.1)
+    x = x0
+    xs = [x]
+    S = length(bf)
+    for i in 1:S
+         # Sampling from guided or unconditional?
+        kg = exp(logweight(x,bf[i]))
+        λ = kg/(kg + ϵ)
+        x = rand()<λ ? guide(x, bf[i], p) : forward(x, p)  
+        push!(xs, x)
+        # also compute logweight and save
+    end
+    xs
+end
